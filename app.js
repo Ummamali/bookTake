bd764337b583ed5e8a4e69a39658512a3b9b68a1
Ummam Computer <ummamali1@gmail.com>
2020-12-08 12:38:09 +0500

    Designed the book class

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // This is the main application file
 
+// Major DOM elements
 let flash = new MessageContainer(".flash", 'flash-in', 'flash-out');
+let bookTable = new TableContainer('#book-table tbody', "book-in", "book-out", 5000);
